57d898967e13724a20cc5399ff34ec36a70b3d18
jawaun <jawaun.brown95@gmail.com>
2024-07-03 13:03:08 -0400

    added getDragAfterElement helper method

--- scripts.js
+++ scripts.js
@@ -58,6 +58,20 @@ function deleteNote(index) {
     });
 }
 
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.note-item:not(.dragging)')];
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child };
+        } else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
+}
+
 function createNoteElement(note, index) {
     // Create a div as the container for each note item
     const div = document.createElement("div");
